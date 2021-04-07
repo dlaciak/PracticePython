785e728e338b70033cafe060fe9ed2afa627f6c6
dlaciak <dawidlaciak@gmail.com>
2021-04-07 17:26:19 +0200

    hjhehe

--- 10.py
+++ 10.py
@@ -1,4 +1,4 @@
-'program z cw5 tylko z użyciem List Overlap Comprehensions
+#program z cw5 tylko z użyciem List Overlap Comprehensions
 
 import random
 
@@ -6,7 +6,7 @@ List1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 List2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 NewList = []
 
-[NewList.append(element) for element in List1 for element2 in List2 if element == element2 and element not in NewList]
+[NewList.append(element) for element in List1 if element in List2 and element not in NewList]
 print(NewList)
 
 RandomQuantityForList3 = random.randint(1,10)
@@ -21,6 +21,6 @@ NewList2 = []
 print('Pierwsza losowa lista: {}' .format(List3))   
 print('Druga losowa lista: {}' .format(List4))
 
-[NewList2.append(element) for element in List3 for element2 in List4 if element == element2 and element not in NewList2]
+[NewList2.append(element) for element in List3 if element in List4 and element not in NewList2]
 print('Wspólne warttości dwóch losowych list: {}' .format(NewList2))
 
--- 13.py
+++ 13.py
@@ -3,10 +3,8 @@ def InputNumber(Text):
 
 def Fibonacci(Number):
     List = [1, 1]
-    for i in range(Number-2):
-        List.append(List[i] + List[i+1])
+    [List.append(List[i] + List[i+1]) for i in range(Number-2)]
     return List
-
 
 Number = InputNumber('Wprowadź liczbę naturalną: ')
 List = Fibonacci(Number)
